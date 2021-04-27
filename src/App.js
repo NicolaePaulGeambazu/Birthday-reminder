e3cde3b7409ce75eba63b3ba8b144dc5ff64a707
paulgsm <58441846+paulgsm@users.noreply.github.com>
2021-04-27 10:54:44 +0100

    done this one

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import data from './data/data';
+import "./App.css";
+import List from "./list.js";
+
+import './index.css';
 
 function App() {
+  const [people, setPeople] = useState(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className='container'>
+        <h3>{people.length} birthday today</h3>
+        <List people={people} />
+        <button onClick={() => setPeople([])}>clear all</button>
+      </section>
+    </main>
   );
 }
 
---
+++ src/data/data.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    id:1,
+    name:'Catalin Jiga',
+    age: 30,
+    image: '',
+  },
+  {
+    id:2,
+    name:'CAMELIA BADEA',
+    age: 24,
+    image: '',
+  },
+  {
+    id:3,
+    name:'LUCAS THEODOR ',
+    age: 1,
+    image:'' ,
+  },
+  {
+    id:4,
+    name:'PAUL NICOLAE',
+    age: 27,
+    image:'', 
+  }
+]
